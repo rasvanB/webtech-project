05b10be877b1b6a3140d5f306488c8e83496a47e
rasvanB <not.obelix@gmail.com>
2023-01-14 17:08:26 +0200

    added emotion for css-in-js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,42 @@
+import { css } from "@emotion/css";
+import "./app.scss";
+
 function App() {
-  return <div className="App">APP</div>;
+  return (
+    <div className="App">
+      <div
+        className={css`
+          height: 100%;
+          width: 100%;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        `}
+      >
+        <div
+          className={css`
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            padding: 20px;
+            background-color: #1d1d23;
+          `}
+        >
+          <h1>Select your role</h1>
+          <div
+            className={css`
+              display: flex;
+              justify-content: space-around;
+              margin-bottom: 20px;
+            `}
+          >
+            <div>professor</div>
+            <div>student</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
