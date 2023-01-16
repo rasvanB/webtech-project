51f79f8caacdc3a001a41135679135f35c4ad6dc
rasvanB <not.obelix@gmail.com>
2023-01-16 13:51:40 +0200

    created all sequelize models

--- backend/utils/sequelize.js
+++ backend/utils/sequelize.js
@@ -7,10 +7,58 @@ const sequelize = new Sequelize({
 const Activity = sequelize.define("activity", {
   date: Sequelize.DATE,
   description: Sequelize.STRING,
-  accessCode: Sequelize.STRING,
+  accessCode: Sequelize.INTEGER,
+  title: Sequelize.STRING,
+  professorName: {
+    type: Sequelize.STRING,
+    references: {
+      model: "professors",
+      key: "name",
+    },
+  },
+});
+
+const Professor = sequelize.define("professor", {
+  name: {
+    type: Sequelize.STRING,
+    primaryKey: true,
+    unique: true,
+    allowNull: false,
+  },
+});
+
+const Feedback = sequelize.define("feedback", {
+  description: Sequelize.STRING,
+  date: Sequelize.DATE,
+  type: Sequelize.STRING,
+  activityId: {
+    type: Sequelize.INTEGER,
+    references: {
+      model: "activities",
+      key: "id",
+    },
+  },
+});
+
+Professor.hasMany(Activity, {
+  foreignKey: "professorName",
+});
+
+Activity.belongsTo(Professor, {
+  foreignKey: "professorName",
+});
+
+Feedback.belongsTo(Activity, {
+  foreignKey: "activityId",
+});
+
+Activity.hasMany(Feedback, {
+  foreignKey: "activityId",
 });
 
 module.exports = {
   sequelize,
   Activity,
+  Professor,
+  Feedback,
 };
