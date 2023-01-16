7234a47186fd8034580e24bc42a6bff3df8a3ed6
rasvanB <not.obelix@gmail.com>
2023-01-16 13:50:50 +0200

    added middleware

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const morgan = require("morgan");
+const { sequelize } = require("./utils/sequelize");
+const unknownEndpoint = require("./middleware/unknown-endpoint");
+const cors = require("cors");
+const professorsRouter = require("./controllers/professors");
 const app = express();
 const port = 3000;
 
+app.use(express.json());
 app.use(morgan("tiny"));
-
+app.use(cors());
+app.use("/api/professors/", professorsRouter);
+app.use(unknownEndpoint);
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+sequelize.sync({ force: true }).then(() => {
+  console.log("Database synced");
+  app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+  });
 });
