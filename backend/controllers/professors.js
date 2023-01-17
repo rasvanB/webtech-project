16dea94d2645c075e9123c6a1afc6209028e0033
rasvanB <not.obelix@gmail.com>
2023-01-17 10:40:56 +0200

    finished all methods for professors router

--- backend/controllers/professors.js
+++ backend/controllers/professors.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const { Professor } = require("../utils/sequelize");
+const { Professor, Activity } = require("../utils/sequelize");
 const professorsRouter = Router();
 
 professorsRouter.post("/", async (req, res) => {
@@ -16,6 +16,8 @@ professorsRouter.post("/", async (req, res) => {
     } catch (error) {
       res.status(400).json({ error: error.errors[0].message });
     }
+  } else {
+    res.status(400).json({ error: "Missing fields" });
   }
 });
 
@@ -23,15 +25,63 @@ professorsRouter.get("/:name", async (req, res) => {
   let { name } = req.params;
   name = name.split("%20").join(" ");
   try {
-    const professor = await Professor.findOne({ where: { name } });
+    const professor = await Professor.findOne({
+      where: { name },
+      include: {
+        model: Activity,
+        include: "feedbacks",
+      },
+    });
     if (professor) {
       res.status(201).json(professor.dataValues);
     } else {
       res.status(404).json({ error: "Professor not found" });
     }
   } catch (error) {
-    res.status(400).json({ error: error.errors[0].message });
+    res.status(400).json({ error });
   }
 });
 
+professorsRouter.get("/:name/activities", async (req, res) => {
+  let { name } = req.params;
+  name = name.split("%20").join(" ");
+  try {
+    const professor = await Professor.findOne({
+      where: { name },
+      include: {
+        model: Activity,
+        include: "feedbacks",
+      },
+    });
+    if (professor) {
+      res.status(201).json(professor.activities);
+    } else {
+      res.status(404).json({ error: "Professor not found" });
+    }
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+});
+
+professorsRouter.post("/:name/activities", async (req, res) => {
+  let { name } = req.params;
+  name = name.split("%20").join(" ");
+  const { title, description, accessCode, date } = req.body;
+  if (title && description && accessCode && date) {
+    try {
+      const createdActivity = await Activity.create({
+        title,
+        date,
+        description,
+        accessCode,
+        professorName: name,
+      });
+      res.status(201).json(createdActivity.dataValues);
+    } catch (error) {
+      res.status(400).json({ error });
+    }
+  } else {
+    res.status(400).json({ error: "Missing fields" });
+  }
+});
 module.exports = professorsRouter;
